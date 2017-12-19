0d633968379eee7b1116e09c36b509fdc2daf628
KRIVIERE <kvin_riviere@yahoo.fr>
2017-12-19 11:48:58 +0100

    Correction de l'utilisateur et de ses privilèges

--- php/bdd.sql
+++ php/bdd.sql
@@ -1,6 +1,3 @@
 CREATE DATABASE IF NOT EXISTS projetTrello
 
-CREATE USER 'admin'@'localhost' IDENTIFIED BY 'admin';
-GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP
-    ON Tables, Users, Tags
-    to 'admin'@'localhost';
+GRANT ALL PRIVILEGES ON projetTrello.* TO 'admin'@'localhost' IDENTIFIED BY 'admin';
